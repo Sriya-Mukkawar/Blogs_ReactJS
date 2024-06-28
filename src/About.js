b6dc455ffbd64faf175d42c8c96ca1c3ba8c3160
Mukkawar Sriya <mukkawarsriya@qurinomsolutions.com>
2024-06-28 19:09:00 +0530

    first-comit

---
+++ src/About.js
@@ -0,0 +1,13 @@
+const About = () => {
+    return (
+      <main className="About">
+        <h2>About</h2>
+        <p style={{ marginTop: "1rem" }}>
+          This blog app is a project in the Learn React tutorial series. It is designed to help beginners and intermediate developers understand the fundamentals of building a modern web application using React. Throughout this series, you will learn how to create components, manage state, handle events, and interact with APIs. Additionally, the tutorial covers essential concepts such as routing with React Router, styling components with CSS and styled-components, and implementing responsive design. By the end of the tutorial, you will have a solid understanding of React and be equipped with the skills to build and deploy your own web applications.
+        </p>
+      </main>
+    )
+  }
+  
+  export default About
+  
